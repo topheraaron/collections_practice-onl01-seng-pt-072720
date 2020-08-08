e20448e4f069d58e843c6957941fb5d80cbc49c5
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-08 01:48:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |n|
+    n[2] = "$"
+    n
+  end
+end
+
+def find_a(array)
+  array.select do |word|
+    word.chr == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, num|
+    num + sum
+  end
+end
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+  end
+end
